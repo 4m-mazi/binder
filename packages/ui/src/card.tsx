127c1c4ab0e5e1845071d469469f2446446b465f
hulk510 <dorian.51069@gmail.com>
2024-01-12 17:05:36 +0900

    style: prettier

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -13,8 +13,8 @@ export function Card({
     <a
       className={className}
       href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
+      rel='noopener noreferrer'
+      target='_blank'
     >
       <h2>
         {title} <span>-&gt;</span>
